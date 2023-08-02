11df399a963613968f6c6ad34b157212b61bb4e3
Sungwook Choi <noname2048@users.noreply.github.com>
2023-08-03 00:24:53 +0900

    feat: create endpoint for join last temperature (#25)
    * fix: fix broadcast with gather, subscribe and unsubscribe
    
    * feat:create endpoint for append last temperature

--- backend/app/api/sensor.py
+++ backend/app/api/sensor.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Body, Path
-from sqlalchemy import select
+from fastapi import APIRouter, Body, Path, Query
+from pydantic import UUID4
+from sqlalchemy import select, text
 
 from ..database import SessionLocal
 from ..models import Sensor
-from ..schemas import SensorCreateRequest
+from ..schemas import SensorCreateRequest, SensorWithLastResponse
 
 router = APIRouter()
 
@@ -16,8 +17,38 @@ async def list_sensor():
     return sensors
 
 
-@router.get("/{uuid}")
-async def retrieve_sensor(uuid: str = Path(...)):
+@router.get("/last", response_model=list[SensorWithLastResponse])
+async def list_sensor_with_last_record():
+    with SessionLocal() as session:
+        session.query(Sensor)
+        query = text(
+            """
+            select s.*, x.temperature, x.humidity, x.created_at as last
+            from sensor s
+                left join
+                (
+                    select *
+                    from (
+                        select *,
+                            row_number() over (
+                                partition by uuid
+                                order by created_at desc
+                            ) as rank
+                        from sensor_record
+                    ) t
+                    where rank = 1
+                    order by created_at desc
+                ) x on s.uuid = x.uuid
+            order by s.created_at asc;
+            """
+        )
+        rows = session.execute(query).all()
+        results = [row._mapping for row in rows]
+    return results
+
+
+@router.get("/uuid/{uuid}")
+async def retrieve_sensor(uuid: UUID4 = Path(...)):
     with SessionLocal() as session:
         stmt = select(Sensor).where(Sensor.uuid == uuid)
         sensor = session.execute(stmt).scalar_one_or_none()
