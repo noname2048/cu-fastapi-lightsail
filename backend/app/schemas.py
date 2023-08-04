892bf8b41ada11890f39d7c1631d447319206c82
Sungwook Choi <sungwook.csw@gmail.com>
2023-08-04 16:35:21 +0900

    feat: create field for alias with sensor

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -6,11 +6,13 @@ from pydantic import UUID4, BaseModel, ConfigDict
 
 class SensorCreateRequest(BaseModel):
     name: str
+    alias: Optional[str]
 
 
 class Sensor(BaseModel):
     uuid: str
     name: str
+    alias: Optional[str]
     created_at: datetime
 
 
@@ -22,6 +24,7 @@ class SensorRecordCreateRequest(BaseModel):
 
 class SensorWithLastResponse(Sensor):
     uuid: UUID4
+    alias: Optional[str]
     temperature: Optional[float]
     humidity: Optional[float]
     last: Optional[datetime]
