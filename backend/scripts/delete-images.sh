ae83820acd41bb74ae32bf36c17e8d1e05a514ca
Sungwook Choi <noname2048@users.noreply.github.com>
2023-08-19 22:53:37 +0900

    [#37] refactor cron job

--- backend/scripts/delete-images.sh
+++ backend/scripts/delete-images.sh
@@ -1,9 +1,18 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-images=$(aws lightsail get-container-images  --service-name cu-container-service-1 | jq -r '.containerImages[] | .image')
-images_to_delete=$(echo $images | tail -n +5)
+aws lightsail get-container-images  \
+    --profile lightsail \
+    --service-name cu-container-service-1 \
+    | jq -r '.containerImages[] | .image' \
+    | tail -n +5
+    > images.txt
+images=$(cat images.txt)
+# tail 은 echo 를 통해 pipeline 으로 넘겨줄 때,
+# -n +5 옵션이 제대로 작동하지 않음
 for image in $images
 do
     echo "delete $image"
-    aws lightsail delete-container-image --service-name cu-container-service-1 --image $image
+    # aws lightsail delete-container-image --service-name cu-container-service-1 --image $image
 done
+
+wc -l images.txt | awk '{print $1}' > count.txt
